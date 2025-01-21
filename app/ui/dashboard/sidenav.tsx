6b863539b8e7bae81d1b37d422188726347905c0
Yeampier J. Huerta <yjhuerta@grupoctg.local>
2025-01-21 14:19:56 -0400

    feat: streaming

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,60 +1,30 @@
-"use client";
-
-import Image from "next/image";
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-  ArrowLeftIcon,
-} from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/dashboard/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
-const links = [
-  { name: "Home", href: "/dashboard", icon: HomeIcon },
-  { name: "users", href: "/dashboard/users", icon: UserGroupIcon },
-  {
-    name: "invoices",
-    href: "/dashboard/invoices",
-    icon: DocumentDuplicateIcon,
-  },
-  {
-    name: "Sign out",
-    href: "/",
-    icon: ArrowLeftIcon,
-  },
-];
-
-export default function Page() {
-  const pathname = usePathname();
-
+export default function SideNav() {
   return (
-    <div className="grid size-full grid-rows-5 gap-1">
-      <div className="bg-blue-400 w-full">
-        <Image
-          src="/acme.png"
-          width={50}
-          height={200}
-          className="hidden md:block h-10 w-15 top-[7.5vw] absolute"
-          alt=""
-        />
-      </div>
-
-      {links.map((link) => (
-        <div key={link.name} className="px-[2vw] py-[1vw] h-[3vh]">
-          <Link
-            href={link.href}
-            className={
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3" +
-              (pathname === link.href ? " bg-sky-100 text-blue-600" : "")
-            }
-          >
-            <link.icon className="h-6 w-6" />
-            {link.name}
-          </Link>
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40">
+          <AcmeLogo
+          />
         </div>
-      ))}
-      
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
